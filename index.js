aee0e7e1ac3cf0306cc0ff7711300333375788b3
kumarmithunc <mithun9545@gmail.com>
2019-10-21 05:54:35 -0800

    1 file modified

--- index.js
+++ index.js
@@ -4,18 +4,34 @@ var momenttz= require('moment-timezone');
 //console.log(moment().format());
 //console.log(moment(new Date(1570690654007)).format('MMMM Do YYYY, h:mm:ss a'));
 
-var anchorage = moment.tz(new Date(1570690654007), "America/Anchorage");
-var kolkata = anchorage.clone().tz('Asia/Kolkata');
-
-console.log('anchorage : ',anchorage.format('MM/DD/YYYY h:mm:ss a'));
-console.log('asia/kolkata : ',kolkata.format('MM/DD/YYYY h:mm:ss a'));
-
-console.log(new Date('10/21/2019 08:00 AM'));
-console.log(new Date('10/21/2019 08:00 PM'));
-
-//var a = moment.tz(new Date('10/21/2019 08:00 AM'),"Asia/Kolkata");
-//console.log(a);
-console.log(moment(new Date('2019-10-21T16:00:00.000Z')).format('MM/DD/YYYY hh::mm a'))
-console.log(moment(new Date('2019-10-22T04:00:00.000Z')).format('MM/DD/YYYY hh::mm a'))
+//var anchorage = moment.tz(new Date(1570690654007), "America/Anchorage");
+var kolkata = moment.tz(new Date('10/21/2019 08:00 AM'), "Asia/Kolkata");
+console.log('kolkata : ',kolkata.format('MM/DD/YYYY h:mm:ss a'));
 
 
+// var anchorage = kolkata.clone().tz('America/Anchorage');
+// console.log('anchorage : ',anchorage.format('MM/DD/YYYY h:mm:ss a'));
+
+
+// console.log(new Date('10/21/2019 08:00 AM'));
+// console.log(new Date('10/21/2019 08:00 PM'));
+
+// //var a = moment.tz(new Date('10/21/2019 08:00 AM'),"Asia/Kolkata");
+// //console.log(a);
+
+
+// console.log(moment(new Date('2019-10-21T16:00:00.000Z')).format('MM/DD/YYYY hh::mm a'))
+// //console.log(moment(new Date('2019-10-22T04:00:00.000Z')).format('MM/DD/YYYY hh::mm a'))
+
+
+// console.log('-------');
+// //from date
+// var kolkata1 = moment.tz(new Date('10/21/2019 08:00 AM'), "Asia/Kolkata");
+// //to date : use browser timezone
+// var anchorage1 = kolkata1.clone().tz('America/Anchorage');
+// console.log(anchorage1.format('MM/DD/YYYY hh::mm a'))
+console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
+
+
+///https://flaviocopes.com/momentjs/
